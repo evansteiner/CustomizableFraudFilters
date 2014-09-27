c58eb015142d887a2eb8793f938c9ebf35915419
Evan Steiner <evan.d.steiner@gmail.com>
2014-09-26 22:13:18 -0400

    basic check of address match as proof of concept

--- code/community/Foundation/CustomizableFraudFilters/Helper/Data.php
+++ code/community/Foundation/CustomizableFraudFilters/Helper/Data.php
@@ -1,13 +1,27 @@
 <?php
 class Foundation_CustomizableFraudFilters_Helper_Data extends Mage_Core_Helper_Abstract
 {
+  public function checkZipCode($order) {
+    $billingAddress = $order->getBillingAddress();
+    $shippingAddress = $order->getShippingAddress();
+    $billingZip = $billingAddress["postcode"];
+    $shippingZip = $shippingAddress["postcode"];
+    Mage::log($billingZip);
+    Mage::log($shippingZip); 
+    if ($billingZip != $shippingZip) {
+    	Mage::log("transition fired");
+      Mage::helper('customizablefraudfilters')->applyFraudFlag($order);
+    }
+  }
+
+
   public function applyFraudFlag($order){
-                $state = 'holded';
-                $status = 'manual_review';
-                $comment = 'Flagged for manual review.';
-                $isCustomerNotified = false;
-                $order->setState($state, $status, $comment, $isCustomerNotified);
-                $order->save(); 
+    $state = 'holded';
+    $status = 'manual_review';
+    $comment = 'Flagged for manual review.';
+    $isCustomerNotified = false;
+    $order->setState($state, $status, $comment, $isCustomerNotified);
+    $order->save(); 
   }
 }
 	 
--- code/community/Foundation/CustomizableFraudFilters/Model/Observer.php
+++ code/community/Foundation/CustomizableFraudFilters/Model/Observer.php
@@ -3,6 +3,7 @@ class Foundation_CustomizableFraudFilters_Model_Observer {
   public function applyFilters(Varien_Event_Observer $observer) {
     $orderId = Mage::getSingleton('checkout/session')->getLastRealOrderId();
     $order = Mage::getModel('sales/order')->loadByIncrementId($orderId);
-    Mage::helper('customizablefraudfilters')->applyFraudFlag($order);
+    Mage::helper('customizablefraudfilters')->checkZipCode($order);
+    //Mage::helper('customizablefraudfilters')->applyFraudFlag($order);
   }
 }
