0dc30c4f75c4a3ac2fad3634902554be0c9acacb
Evan Steiner <evan.d.steiner@gmail.com>
2014-09-27 17:27:21 -0400

    set up acl permissions for saving back end attributes

--- code/community/Foundation/CustomizableFraudFilters/Model/Observer.php
+++ code/community/Foundation/CustomizableFraudFilters/Model/Observer.php
@@ -3,7 +3,10 @@ class Foundation_CustomizableFraudFilters_Model_Observer {
   public function applyFilters(Varien_Event_Observer $observer) {
     $orderId = Mage::getSingleton('checkout/session')->getLastRealOrderId();
     $order = Mage::getModel('sales/order')->loadByIncrementId($orderId);
+    //begin filters
+    $a = Mage::getStoreConfig('customizablefraudfilters/filter_options/zip_code_flag');
+    Mage::log("zip code is set to: ".$a);
     Mage::helper('customizablefraudfilters')->checkZipCode($order);
-    //Mage::helper('customizablefraudfilters')->applyFraudFlag($order);
+
   }
 }
