6d154c64486d7985f1e125b41fb90861918abfd0
Evan Steiner <evan.d.steiner@gmail.com>
2014-09-26 20:33:41 -0400

    trigger all orders as requiring manual review

--- code/community/Foundation/CustomizableFraudFilters/Helper/Data.php
+++ code/community/Foundation/CustomizableFraudFilters/Helper/Data.php
@@ -1,5 +1,19 @@
 <?php
 class Foundation_CustomizableFraudFilters_Helper_Data extends Mage_Core_Helper_Abstract
 {
+  public function applyFraudFlag($order){
+    // $order->setState("fraud_review");
+    // $order->setStatus("New");
+    // $order->save();
+    
+    //$order = Mage::getModel('sales/order')->loadByIncrementId($order_update->getIncrementId());
+                $state = 'holded';
+                $status = 'manual_review';
+                $comment = 'Flagged for manual review.';
+                $isCustomerNotified = false;
+                $order->setState($state, $status, $comment, $isCustomerNotified);
+                $order->save(); 
+     
+  }
 }
 	 
--- code/community/Foundation/CustomizableFraudFilters/Model/Observer.php
+++ code/community/Foundation/CustomizableFraudFilters/Model/Observer.php
@@ -1,6 +1,8 @@
 <?php
 class Foundation_CustomizableFraudFilters_Model_Observer {
   public function applyFilters(Varien_Event_Observer $observer) {
-  Mage::log("observer is firing");
+    $orderId = Mage::getSingleton('checkout/session')->getLastRealOrderId();
+    $order = Mage::getModel('sales/order')->loadByIncrementId($orderId);
+    Mage::helper('customizablefraudfilters')->applyFraudFlag($order);
   }
 }
