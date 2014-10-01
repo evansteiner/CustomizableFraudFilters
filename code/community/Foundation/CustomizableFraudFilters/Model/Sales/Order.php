a168dc6f7a9fc6d422df5d659d1f0fc4802f8f9d
Evan Steiner <evan.d.steiner@gmail.com>
2014-09-30 22:25:23 -0400

    fixed issue with front end status lables (git to the rescue)

--- code/community/Foundation/CustomizableFraudFilters/Model/Sales/Order.php
+++ code/community/Foundation/CustomizableFraudFilters/Model/Sales/Order.php
@@ -3,7 +3,7 @@ class Foundation_CustomizableFraudFilters_Model_Sales_Order extends Mage_Sales_M
   public function getStatusLabel() {
     $status = $this->getConfig()->getStatusLabel($this->getStatus());
     if($status == "Manual Review Required"){
-      $replaceStatus = Mage::getStoreConfig('customizablefraudfilters/alerts/frontend_status');
+      $replaceStatus = Mage::getStoreConfig('customizablefraudfilters/general_settings/frontend_status');
       if($replaceStatus != ""){
         $status = $replaceStatus;
       }
